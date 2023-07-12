fe880f39d1c01e7f45d7dbc9418318463d7b65b6
Matthieu Mota <matthieumota@gmail.com>
2023-07-12 12:26:47 +0200

    Begin project

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        @section('title')
+            Webflix
+        @show
+    </title>
+</head>
+<body>
+    <nav>
+        <a href="/">Accueil</a>
+        <a href="/fiorella">Fiorella</a>
+        <a href="/fiorella?color=blue">Fiorella bleue</a>
+        <a href="/fiorella/emma">Fiorella et Emma</a>
+        <a href="/fiorella/paul">Fiorella et Paul</a>
+    </nav>
+
+    @yield('content')
+
+    <footer>
+        {{ date('Y') }}
+    </footer>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +16,23 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'Webflix';
+
+    return view('home', [
+        'title' => $title,
+        'numbers' => [1, 2, 3],
+    ]);
+});
+
+Route::get('/fiorella/{friend?}', function (Request $request, string $friend = null) {
+    // Pour les paramètres get...
+    dump($_GET['color'] ?? null); // Ancienne méthode...
+    dump($request->input('color', 'rose')); // Nouvelle méthode...
+    dump(request('color')); // Méthode rapide...
+
+    return view('fiorella', [
+        'age' => Carbon::parse('2019-12-31')->age,
+        'color' => $request->input('color', 'rose'),
+        'friend' => ucfirst($friend),
+    ]);
 });
